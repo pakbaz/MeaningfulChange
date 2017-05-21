1a3acb8e700178cf8b42a0dc645a513762515bab
Sam Pakbaz <sam.pakbaz@neudesic.com>
2017-05-21 00:45:50 -0500

    custom map, loaded census data, donate

--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -1,15 +1,17 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+import { Http, Response } from '@angular/http';
+import { Donate } from '../donate/donate';
 
 import {
- GoogleMaps,
- GoogleMap,
- GoogleMapsEvent,
- LatLng,
- CameraPosition,
- MarkerOptions,
- Marker
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  LatLng,
+  CameraPosition,
+  MarkerOptions,
+  Marker
 } from '@ionic-native/google-maps';
 import { CensusServiceProvider } from "../../providers/census-service/census-service";
 
@@ -23,31 +25,35 @@ declare var google;
 export class Report {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-
+  currentPosition: Geoposition;
   private clientCount: number = 0;
-  constructor(public navCtrl: NavController,private geolocation:Geolocation, private googleMaps: GoogleMaps, private censusService: CensusServiceProvider) {
+  constructor(public navCtrl: NavController, 
+  private geolocation: Geolocation, 
+  private googleMaps: GoogleMaps, 
+  private censusService: CensusServiceProvider,
+  private http: Http) {
 
   }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     this.loadMap();
-    
+
   }
- 
-  loadMap(){
- 
+
+  loadMap() {
+
     this.geolocation.getCurrentPosition().then((position) => {
- 
+      this.currentPosition = position;
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
+
       let mapOptions = {
         center: latLng,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       }
- 
+
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-      
+
       this.addMarker();
 
     }, (err) => {
@@ -55,32 +61,75 @@ export class Report {
     });
   }
 
-  addMarker(){
- 
-  let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: this.map.getCenter()
-  });
- 
+  addMarker() {
 
-  let content = "You are Here";          
- 
-  this.addInfoWindow(marker, content);
- 
+    new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    this.censusService.load()
+      .then(data => {
+        for (var mark of data) {
+          let marker = new google.maps.Marker({
+            map: this.map,
+            icon: {
+              //path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+              path: google.maps.SymbolPath.CIRCLE,
+              scale: 4,
+              fillOpacity: 0.5,
+              fillColor: 'lightblue',
+              strokeWeight: 2,
+              strokeOpacity: 0.8,
+              strokeColor: 'darkblue'
+            },
+            animation: google.maps.Animation.DROP,
+            position: { lat: mark.ObservationLatitude, lng: mark.ObservationLongitude }
+          });
+          let content = '<p>' + mark.ObservationGroup + ' persons has been reported here</p>';
+          this.addInfoWindow(marker, content);
+        }
+      });
+
+  }
+
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
+
+  ReportnDonate(){
+    this.Report();
+    this.navCtrl.push(Donate); 
+  }
+
+  Report(){
+    var url = 'https://hessersapi.azurewebsites.net/api/InsertObservationCensusRecord';
+    var date = new Date();
+ var reportJson = 
+    {
+      "userName" : "spakbaz",
+      "latitude" : this.currentPosition.coords.latitude,
+      "longitude" : this.currentPosition.coords.longitude,
+      "people" : this.clientCount,
+      "paymentCode" : "ABCD",
+      "donationDateTime" : date.toString()
+    }
+    this.http.post(url, reportJson)
+    .map(res => res.json())
+    .subscribe(data => {console.info('reported')});
+
+  }
+
 }
 
-addInfoWindow(marker, content){
- 
-  let infoWindow = new google.maps.InfoWindow({
-    content: content
-  });
- 
-  google.maps.event.addListener(marker, 'click', () => {
-    infoWindow.open(this.map, marker);
-  });
- 
-}
-
-}
 
