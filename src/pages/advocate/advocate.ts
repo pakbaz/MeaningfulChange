4321b3b01a6d660232d12d168f81588c735e681f
Boone Watkins <boone.watkins@gmail.com>
2017-05-21 08:45:05 -0500

    no message

--- src/pages/advocate/advocate.ts
+++ src/pages/advocate/advocate.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-advocate',
@@ -8,8 +8,15 @@ import { NavController } from 'ionic-angular';
 
 export class Advocate {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public platform: Platform) {
 
+  }
+
+  OpenPledgeSite()
+  {
+    var url = "https://visitor.r20.constantcontact.com/manage/optin?v=001dbhkIZY57-BdIfJJpvgOsvWXPOxEo2jubHG3z4eP7IhynRzVbs6gf9PphfNhZBtcHnsyAyaPAgnoH03eBs3Vx2J3VA_nseHedhUi-rRrPEkSMlP_kkfdklbAehLEDhii7xCItr0vAfvjcDjV0HZ-y1q_3hV5HZDkbnGzRf_iEx40QaUhtsjgaUEShGJ8KWr9";
+
+    this.platform.ready().then(() => { open(url, '_blank', 'location = yes');})
   }
 
 }
